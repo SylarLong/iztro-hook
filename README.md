b3a66939407e6177a27900a51d10f434ff5065c9
Sylar <alsogood@qq.com>
2023-09-20 00:05:17 +0800

    add UT

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # iztro-hook
-a hook of iztro
+
+基于 [`iztro`](https://github.com/SylarLong/iztro) 实现的 `react hook`。
+用于获取紫微斗数星盘数据，运限数据（大小限，流年，流月，流日，流时等）。详细功能请查看 [iztro开发文档](https://docs.iztro.com)
+
+## 安装 (installation)
+
+```
+npm install iztro iztro-hook -S
+```
+
+## 参数
+
+```
+export type IztroInput = {
+  birthday: string;
+  birthTime: number;
+  gender: GenderName;
+  birthdayType: 'lunar' | 'solar';
+  isLeapMonth?: boolean;
+  fixLeap?: boolean;
+};
+```
+
+- `GenderName` 的定义在 [iztro 开发文档 -> GenderName](https://docs.iztro.com/type-definition.html#gendername) 里。
+
+## hook 定义
+
+```
+type useIztro = (input: IztroInput) => {
+  astrolabe: FunctionalAstrolabe | undefined;
+  horoscope: Horoscope | undefined;
+  setHoroscope: (date: string | Date, hour?: number) => void;
+}
+```
+
+- `FunctionalAstrolabe` 的定义在 [iztro 开发文档 -> FunctionalAstrolabe](https://docs.iztro.com/posts/astrolabe.html#functionalastrolabe) 里。
+- `Horoscope` 的定义在 [iztro 开发文档 -> Horoscope](https://docs.iztro.com/type-definition.html#horoscope) 里。
+
+## 使用
+
+该 `hook` 只能在 `react` 组件中使用，如果你的项目不是使用 `react` 框架开发的，请使用 [`iztro`](https://github.com/SylarLong/iztro) 自行封装。
+
+```ts
+
+import React from 'react';
+import { useIztro } from 'iztro-hook';
+
+export default App() {
+  const { astrolabe, horoscope, setHoroscope } = useIztro({
+    birthday: '2000-18-16',
+    birthdayType: 'solar',
+    birthTime: 2,
+    gender: '女',
+    fixLeap: true,
+  });
+
+
+  return (
+    <>在此绑定数据进行渲染</>
+  );
+}
+
+```
+
+>如果你想了解更多关于紫微斗数的知识或者本开源库的信息，可以查看 [iztro](https://github.com/SylarLong/iztro)。如果好用记得给我一个⭐️鼓励哦～
---
+++ __tests__/index.test.ts
@@ -0,0 +1,302 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import { useIztro } from '../src/index';
+
+describe('Test useIztro hook', () => {
+  it('useIztro() return undefined', () => {
+    const { result } = renderHook(() =>
+      useIztro({
+        birthday: '2000-18-16',
+        birthdayType: 'solar',
+        birthTime: 2,
+        gender: '女',
+        fixLeap: true,
+      }),
+    );
+
+    expect(result.current.astrolabe).toBeUndefined();
+    expect(result.current.horoscope).toBeUndefined();
+  });
+
+  it('useIztro() solar', async () => {
+    const { result } = renderHook(() =>
+      useIztro({
+        birthday: '2000-8-16',
+        birthdayType: 'solar',
+        birthTime: 2,
+        gender: '女',
+        fixLeap: true,
+      }),
+    );
+
+    const { current } = result;
+
+    expect(current.astrolabe).toHaveProperty('solarDate', '2000-8-16');
+    expect(current.astrolabe).toHaveProperty('lunarDate', '二〇〇〇年七月十七');
+    expect(current.astrolabe).toHaveProperty('chineseDate', '庚辰 甲申 丙午 庚寅');
+    expect(current.astrolabe).toHaveProperty('time', '寅时');
+    expect(current.astrolabe).toHaveProperty('sign', '狮子座');
+    expect(current.astrolabe).toHaveProperty('zodiac', '龙');
+    expect(current.astrolabe).toHaveProperty('earthlyBranchOfSoulPalace', '午');
+    expect(current.astrolabe).toHaveProperty('earthlyBranchOfBodyPalace', '戌');
+    expect(current.astrolabe).toHaveProperty('soul', '破军');
+    expect(current.astrolabe).toHaveProperty('body', '文昌');
+    expect(current.astrolabe).toHaveProperty('fiveElementsClass', '木三局');
+
+    const horoscopeValue = current?.astrolabe?.horoscope('2023-8-19 3:12');
+
+    expect(horoscopeValue).toHaveProperty('solarDate', '2023-8-19');
+    expect(horoscopeValue?.decadal).toHaveProperty('index', 2);
+    expect(horoscopeValue?.decadal).toHaveProperty('heavenlyStem', '庚');
+    expect(horoscopeValue?.decadal).toHaveProperty('earthlyBranch', '辰');
+    expect(horoscopeValue?.decadal).toHaveProperty('palaceNames', [
+      '夫妻',
+      '兄弟',
+      '命宫',
+      '父母',
+      '福德',
+      '田宅',
+      '官禄',
+      '仆役',
+      '迁移',
+      '疾厄',
+      '财帛',
+      '子女',
+    ]);
+    expect(horoscopeValue?.decadal).toHaveProperty('mutagen', ['太阳', '武曲', '太阴', '天同']);
+    expect(horoscopeValue?.age).toHaveProperty('index', 10);
+    expect(horoscopeValue?.age).toHaveProperty('nominalAge', 23);
+    expect(horoscopeValue?.yearly).toHaveProperty('index', 1);
+    expect(horoscopeValue?.yearly).toHaveProperty('heavenlyStem', '癸');
+    expect(horoscopeValue?.yearly).toHaveProperty('earthlyBranch', '卯');
+    expect(horoscopeValue?.yearly).toHaveProperty('palaceNames', [
+      '兄弟',
+      '命宫',
+      '父母',
+      '福德',
+      '田宅',
+      '官禄',
+      '仆役',
+      '迁移',
+      '疾厄',
+      '财帛',
+      '子女',
+      '夫妻',
+    ]);
+    expect(horoscopeValue?.yearly).toHaveProperty('mutagen', ['破军', '巨门', '太阴', '贪狼']);
+    expect(horoscopeValue?.monthly).toHaveProperty('index', 1);
+    expect(horoscopeValue?.monthly).toHaveProperty('heavenlyStem', '庚');
+    expect(horoscopeValue?.monthly).toHaveProperty('earthlyBranch', '申');
+    expect(horoscopeValue?.monthly).toHaveProperty('palaceNames', [
+      '兄弟',
+      '命宫',
+      '父母',
+      '福德',
+      '田宅',
+      '官禄',
+      '仆役',
+      '迁移',
+      '疾厄',
+      '财帛',
+      '子女',
+      '夫妻',
+    ]);
+    expect(horoscopeValue?.monthly).toHaveProperty('mutagen', ['太阳', '武曲', '太阴', '天同']);
+    expect(horoscopeValue?.daily).toHaveProperty('index', 4);
+    expect(horoscopeValue?.daily).toHaveProperty('heavenlyStem', '己');
+    expect(horoscopeValue?.daily).toHaveProperty('earthlyBranch', '酉');
+    expect(horoscopeValue?.daily).toHaveProperty('palaceNames', [
+      '财帛',
+      '子女',
+      '夫妻',
+      '兄弟',
+      '命宫',
+      '父母',
+      '福德',
+      '田宅',
+      '官禄',
+      '仆役',
+      '迁移',
+      '疾厄',
+    ]);
+    expect(horoscopeValue?.daily).toHaveProperty('mutagen', ['武曲', '贪狼', '天梁', '文曲']);
+    expect(horoscopeValue?.hourly).toHaveProperty('index', 6);
+    expect(horoscopeValue?.hourly).toHaveProperty('heavenlyStem', '丙');
+    expect(horoscopeValue?.hourly).toHaveProperty('earthlyBranch', '寅');
+    expect(horoscopeValue?.hourly).toHaveProperty('palaceNames', [
+      '迁移',
+      '疾厄',
+      '财帛',
+      '子女',
+      '夫妻',
+      '兄弟',
+      '命宫',
+      '父母',
+      '福德',
+      '田宅',
+      '官禄',
+      '仆役',
+    ]);
+    expect(horoscopeValue?.hourly).toHaveProperty('mutagen', ['天同', '天机', '文昌', '廉贞']);
+
+    const { horoscope } = result.current;
+
+    expect(horoscope?.age).toHaveProperty('index', 9);
+    expect(horoscope?.age).toHaveProperty('nominalAge', 24);
+
+    act(() => {
+      result.current.setHoroscope('2034-10-19 3:12');
+    });
+
+    expect(result.current.horoscope?.age).toHaveProperty('index', 10);
+    expect(result.current.horoscope?.age).toHaveProperty('nominalAge', 35);
+
+    act(() => {
+      result.current.setHoroscope('2025-10-19', 2);
+    });
+
+    expect(result.current.horoscope?.age).toHaveProperty('index', 7);
+    expect(result.current.horoscope?.age).toHaveProperty('nominalAge', 26);
+  });
+
+  it('useIztro() lunar', async () => {
+    const { result } = renderHook(() =>
+      useIztro({
+        birthday: '2000-7-17',
+        birthdayType: 'lunar',
+        birthTime: 2,
+        gender: '女',
+        fixLeap: true,
+        isLeapMonth: true,
+      }),
+    );
+
+    const { current } = result;
+
+    expect(current.astrolabe).toHaveProperty('solarDate', '2000-8-16');
+    expect(current.astrolabe).toHaveProperty('lunarDate', '二〇〇〇年七月十七');
+    expect(current.astrolabe).toHaveProperty('chineseDate', '庚辰 甲申 丙午 庚寅');
+    expect(current.astrolabe).toHaveProperty('time', '寅时');
+    expect(current.astrolabe).toHaveProperty('sign', '狮子座');
+    expect(current.astrolabe).toHaveProperty('zodiac', '龙');
+    expect(current.astrolabe).toHaveProperty('earthlyBranchOfSoulPalace', '午');
+    expect(current.astrolabe).toHaveProperty('earthlyBranchOfBodyPalace', '戌');
+    expect(current.astrolabe).toHaveProperty('soul', '破军');
+    expect(current.astrolabe).toHaveProperty('body', '文昌');
+    expect(current.astrolabe).toHaveProperty('fiveElementsClass', '木三局');
+
+    const horoscopeValue = current?.astrolabe?.horoscope('2023-8-19 3:12');
+
+    expect(horoscopeValue).toHaveProperty('solarDate', '2023-8-19');
+    expect(horoscopeValue?.decadal).toHaveProperty('index', 2);
+    expect(horoscopeValue?.decadal).toHaveProperty('heavenlyStem', '庚');
+    expect(horoscopeValue?.decadal).toHaveProperty('earthlyBranch', '辰');
+    expect(horoscopeValue?.decadal).toHaveProperty('palaceNames', [
+      '夫妻',
+      '兄弟',
+      '命宫',
+      '父母',
+      '福德',
+      '田宅',
+      '官禄',
+      '仆役',
+      '迁移',
+      '疾厄',
+      '财帛',
+      '子女',
+    ]);
+    expect(horoscopeValue?.decadal).toHaveProperty('mutagen', ['太阳', '武曲', '太阴', '天同']);
+    expect(horoscopeValue?.age).toHaveProperty('index', 10);
+    expect(horoscopeValue?.age).toHaveProperty('nominalAge', 23);
+    expect(horoscopeValue?.yearly).toHaveProperty('index', 1);
+    expect(horoscopeValue?.yearly).toHaveProperty('heavenlyStem', '癸');
+    expect(horoscopeValue?.yearly).toHaveProperty('earthlyBranch', '卯');
+    expect(horoscopeValue?.yearly).toHaveProperty('palaceNames', [
+      '兄弟',
+      '命宫',
+      '父母',
+      '福德',
+      '田宅',
+      '官禄',
+      '仆役',
+      '迁移',
+      '疾厄',
+      '财帛',
+      '子女',
+      '夫妻',
+    ]);
+    expect(horoscopeValue?.yearly).toHaveProperty('mutagen', ['破军', '巨门', '太阴', '贪狼']);
+    expect(horoscopeValue?.monthly).toHaveProperty('index', 1);
+    expect(horoscopeValue?.monthly).toHaveProperty('heavenlyStem', '庚');
+    expect(horoscopeValue?.monthly).toHaveProperty('earthlyBranch', '申');
+    expect(horoscopeValue?.monthly).toHaveProperty('palaceNames', [
+      '兄弟',
+      '命宫',
+      '父母',
+      '福德',
+      '田宅',
+      '官禄',
+      '仆役',
+      '迁移',
+      '疾厄',
+      '财帛',
+      '子女',
+      '夫妻',
+    ]);
+    expect(horoscopeValue?.monthly).toHaveProperty('mutagen', ['太阳', '武曲', '太阴', '天同']);
+    expect(horoscopeValue?.daily).toHaveProperty('index', 4);
+    expect(horoscopeValue?.daily).toHaveProperty('heavenlyStem', '己');
+    expect(horoscopeValue?.daily).toHaveProperty('earthlyBranch', '酉');
+    expect(horoscopeValue?.daily).toHaveProperty('palaceNames', [
+      '财帛',
+      '子女',
+      '夫妻',
+      '兄弟',
+      '命宫',
+      '父母',
+      '福德',
+      '田宅',
+      '官禄',
+      '仆役',
+      '迁移',
+      '疾厄',
+    ]);
+    expect(horoscopeValue?.daily).toHaveProperty('mutagen', ['武曲', '贪狼', '天梁', '文曲']);
+    expect(horoscopeValue?.hourly).toHaveProperty('index', 6);
+    expect(horoscopeValue?.hourly).toHaveProperty('heavenlyStem', '丙');
+    expect(horoscopeValue?.hourly).toHaveProperty('earthlyBranch', '寅');
+    expect(horoscopeValue?.hourly).toHaveProperty('palaceNames', [
+      '迁移',
+      '疾厄',
+      '财帛',
+      '子女',
+      '夫妻',
+      '兄弟',
+      '命宫',
+      '父母',
+      '福德',
+      '田宅',
+      '官禄',
+      '仆役',
+    ]);
+    expect(horoscopeValue?.hourly).toHaveProperty('mutagen', ['天同', '天机', '文昌', '廉贞']);
+
+    const { horoscope } = result.current;
+
+    expect(horoscope?.age).toHaveProperty('index', 9);
+    expect(horoscope?.age).toHaveProperty('nominalAge', 24);
+
+    act(() => {
+      result.current.setHoroscope('2034-10-19 3:12');
+    });
+
+    expect(result.current.horoscope?.age).toHaveProperty('index', 10);
+    expect(result.current.horoscope?.age).toHaveProperty('nominalAge', 35);
+
+    act(() => {
+      result.current.setHoroscope('2025-10-19', 2);
+    });
+
+    expect(result.current.horoscope?.age).toHaveProperty('index', 7);
+    expect(result.current.horoscope?.age).toHaveProperty('nominalAge', 26);
+  });
+});
